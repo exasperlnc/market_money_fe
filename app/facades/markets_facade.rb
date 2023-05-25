18258399106b2b6e34d908aea2cf2fc07e88a419
Logan Cole <emtlogancole@gmail.com>
2023-05-25 11:23:38 -0500

    create market objects from data

--- app/facades/markets_facade.rb
+++ app/facades/markets_facade.rb
@@ -1,5 +1,30 @@
 class MarketsFacade
   def markets
-    
+    results = service.get_markets[:data]
+
+    results.map do |result|
+      Market.new(format_data(result))
+    end
   end
+
+  private
+    def service
+      @_service ||= MarketService.new
+    end
+
+    def format_data(data)
+      attributes = data[:attributes]
+      {
+        id: data[:id],
+        name: attributes[:name],
+        street: attributes[:street],
+        city: attributes[:city],
+        county: attributes[:county],
+        state: attributes[:state],
+        zip: attributes[:zip],
+        lon: attributes[:lon],
+        lat: attributes[:lat],
+        vendor_count: attributes[:vendor_count]
+      }
+    end
 end

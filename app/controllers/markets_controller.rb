92067f337ba7aaae079ccac7d8c400f6e7b6d63b
Logan Cole <emtlogancole@gmail.com>
2023-05-25 11:36:41 -0500

    feat:market index page

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,5 +1,11 @@
 class MarketsController < ApplicationController
+  before_action :set_facade, only: [:index]
   def index
-    @markets = MarketsFacade.markets
+    @markets = @facade.markets
   end
+
+  private 
+    def set_facade
+      @facade = MarketsFacade.new
+    end
 end

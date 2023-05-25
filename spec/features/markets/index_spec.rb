4ad8e4d9a9aa2faa668c42dfd06c31839cd2eac4
Logan Cole <emtlogancole@gmail.com>
2023-05-25 14:26:35 -0500

    market index is now a table

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Market Index Page' do
   it 'shows all markets' do
     visit '/markets'
+    save_and_open_page
 
     expect(page).to have_content("14&U Farmers' Market")
     expect(page).to have_content('Washington')

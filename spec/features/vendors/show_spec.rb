83482eb612cb2aadffc1a5a302942a8d33b999f3
Logan Cole <emtlogancole@gmail.com>
2023-05-27 13:54:07 -0500

    feat: vendor#show page

--- spec/features/vendors/show_spec.rb
+++ spec/features/vendors/show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Vendor Show Page" do
+  before :each do
+    visit '/vendors/55823'
+  end
   describe "Happy path" do
     it 'visits page' do
-      visit '/vendors/55823'
-
-
       expect(current_path).to eq(vendor_path(55823))
     end
 
@@ -13,20 +13,20 @@ RSpec.describe "Vendor Show Page" do
       expect(page).to have_content("The Charcuterie Corner")
     end
 
-    xit 'has contact name' do
-      expect(page).to have_content()
+    it 'has contact name' do
+      expect(page).to have_content("Contact Name: Claudie Langworth III")
     end
 
-    xit 'has contact phone number' do
-      expect(page).to have_content()
+    it 'has contact phone number' do
+      expect(page).to have_content("Phone: 1-147-179-9747")
     end
 
-    xit 'has credit accepted' do
-      expect(page).to have_content('Credit Accepted: YES')
+    it 'has credit accepted' do
+      expect(page).to have_content('Credit Accepted: No')
     end
 
-    xit 'has description' do
-      expect(page).to have_content()
+    it 'has description' do
+      expect(page).to have_content("Description: Vendor selling a variety of artisanal cured meats and sausages.")
     end
   end
 end
